0865e06768ebd3ab5c1fe0173c6f7f8e404af435
maaikeelgersma <maaike.elgersma@gmail.com>
2025-02-12 17:39:13 +0100

    Comment out certain packages to make it run for Maaike

--- gep_primal_dual_main.py
+++ gep_primal_dual_main.py
@@ -4,7 +4,7 @@ import time
 from gep_config_parser import *
 from data_wrangling import dataframe_to_dict
 
-from primal_dual import PrimalDualTrainer
+# from primal_dual import PrimalDualTrainer
 from gep_problem import GEPProblemSet
 from gep_problem_operational import GEPOperationalProblemSet
 from get_gurobi_vars import OptValueExtractor
@@ -135,7 +135,7 @@ def prep_data(args, inputs, N=None, G=None, L=None, train=0.8, valid=0.1, test=0
 def run_PDL(data, args, save_dir):
     # Run PDL
     print("Training the PDL")
-    trainer = PrimalDualTrainer(data, args, save_dir)
+    trainer = PrimalDualTrainer(data, args, save_dir, log=False)
     primal_net, dual_net, stats = trainer.train_PDL()
     return primal_net, dual_net
 
@@ -169,39 +169,39 @@ if __name__ == "__main__":
             with open(os.path.join(save_dir, 'args.dict'), 'wb') as f:
                 pickle.dump(args, f)
 
-        # Prep proble data:
+        # Prep problem data:
         data = prep_data(args, experiment_instance, N=args["N"], G=args["G"], L=args["L"], train=args["train"], valid=args["valid"], test=args["test"], scale=args["scale_problem"], sample_duration=args["sample_duration"], constant_gen_inv=args["operational"])
 
-        target_path = f"outputs/Gurobi/{args['G']}-OPT_TARGETS_T={args['sample_duration']}"
+        # target_path = f"outputs/Gurobi/{args['G']}-OPT_TARGETS_T={args['sample_duration']}"
 
-        if not os.path.exists(target_path):
-            extractor = OptValueExtractor(args["operational"])
-            for t in data.time_ranges:
-                model, solver, time_taken = run_Gurobi_no_bounds(experiment_instance,
-                            t,
-                            data.N,
-                            data.G,
-                            data.L,
-                            data.pDemand,
-                            data.pGenAva,
-                            data.pVOLL,
-                            data.pWeight,
-                            data.pRamping,
-                            data.pInvCost,
-                            data.pVarCost,
-                            data.pUnitCap,
-                            data.pExpCap,
-                            data.pImpCap,
-                            )
-                extractor.extract_values(model)
+        # if not os.path.exists(target_path):
+        #     extractor = OptValueExtractor(args["operational"])
+        #     for t in data.time_ranges:
+        #         model, solver, time_taken = run_Gurobi_no_bounds(experiment_instance,
+        #                     t,
+        #                     data.N,
+        #                     data.G,
+        #                     data.L,
+        #                     data.pDemand,
+        #                     data.pGenAva,
+        #                     data.pVOLL,
+        #                     data.pWeight,
+        #                     data.pRamping,
+        #                     data.pInvCost,
+        #                     data.pVarCost,
+        #                     data.pUnitCap,
+        #                     data.pExpCap,
+        #                     data.pImpCap,
+        #                     )
+        #         extractor.extract_values(model)
 
-            with open(os.path.join("outputs/Gurobi", f"{args['G']}-OPT_TARGETS_T={args['sample_duration']}"), 'wb') as f:
-                pickle.dump(extractor.opt_targets, f)
+        #     with open(os.path.join("outputs/Gurobi", f"{args['G']}-OPT_TARGETS_T={args['sample_duration']}"), 'wb') as f:
+        #         pickle.dump(extractor.opt_targets, f)
                 
-            data.load_targets(target_path)
+        #     data.load_targets(target_path)
 
-            # Run PDL
-            primal_net, dual_net = run_PDL(data, args, save_dir)
+        #     # Run PDL
+        #     primal_net, dual_net = run_PDL(data, args, save_dir)
 
-            data.plot_balance(primal_net, dual_net)
-            data.plot_decision_variable_diffs(primal_net, dual_net)
+        #     data.plot_balance(primal_net, dual_net)
+        #     data.plot_decision_variable_diffs(primal_net, dual_net)
